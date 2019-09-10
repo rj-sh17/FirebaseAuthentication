09eec1f17b09d1651b6c0d517b8d50abffd206cc
rj-sh17 <rajeevsharma751@gmail.com>
2019-09-10 18:22:44 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { AuthService } from './auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireDatabaseModule
+  ],
+  entryComponents: [
+    SignInComponent, SignUpComponent
+  ],
+  providers: [AuthService, AngularFireAuth],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAhlBGWyyzBDOnuCoaxjXcxZppfqjYYomA',
+    authDomain: 'fir-storage-5218a.firebaseapp.com',
+    databaseURL: 'https://fir-storage-5218a.firebaseio.com',
+    projectId: 'fir-storage-5218a',
+    storageBucket: '',
+    messagingSenderId: '70095880870',
+    appId: '1:70095880870:web:09ddb82612cbc4a0daaa67'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
